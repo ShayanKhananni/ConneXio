1774abe466913fe588973d0eee23fa3d802633c0
Shayan Khanani <shani.khanani@gmail.com>
2026-04-17 18:58:03 +0500

    Initial Setup with frontend and backend for connexio

---
+++ backend/src/test/java/com/shayankhanani/Connexio/ConnexioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shayankhanani.Connexio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
